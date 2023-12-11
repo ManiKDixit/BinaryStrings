7cb4666bd640d537d4db2b24654c6335ccfb3449
ManiKDixit <56781890+ManiKDixit@users.noreply.github.com>
2023-12-11 14:49:16 +0530

    Add project files.

---
+++ GoodBinaryString.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryStrings
+{
+    public class GoodBinaryString
+    {
+       public  bool CheckBinaryString(string binaryString)
+        {
+            // Check if string length is even (equal number of 0's and 1's)
+            if (binaryString.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            int onesCount = 0;
+            int zerosCount = 0;
+
+            foreach (char c in binaryString)
+            {
+                if (c == '1')
+                {
+                    onesCount++;
+                }
+                else if (c == '0')
+                {
+                    zerosCount++;
+                }
+                else
+                {
+                    // Invalid character
+                    return false;
+                }
+
+                // Check prefix condition
+                if (onesCount < zerosCount)
+                {
+                    return false;
+                }
+            }
+
+            // All conditions met
+            return true;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//var string1 = Console.ReadLine();
+//Console.WriteLine(GoodBinaryString(string1));
+
+using BinaryStrings;
+
+
+string binaryString1 = "101010";
+string binaryString2 = "00110";
+string binaryString3 = "110";
+
+GoodBinaryString binaryString = new GoodBinaryString();
+Console.WriteLine(binaryString.CheckBinaryString(binaryString1)); //True
+Console.WriteLine(binaryString.CheckBinaryString(binaryString2)); //True
+Console.WriteLine(binaryString.CheckBinaryString(binaryString3)); //False
